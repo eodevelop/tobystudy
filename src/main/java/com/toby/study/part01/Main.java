32d1329df675f392ccee59ff163093ade411e97c
eodevelop <eodevelop@naver.com>
2022-02-25 15:58:04 +0900

    1.6 싱글톤 레지스트리와 오브젝트 스코프 - userDao 를 DaoFactory를 이용한 직접 구현과 ApplicationContext를 통한 구현으로 어떠한 차이가 나는지 알아본다 - ApplicationContext를 이용하여 userDao 생성시 객체의 동일성을 보장해준다.

--- src/main/java/com/toby/study/part01/Main.java
+++ src/main/java/com/toby/study/part01/Main.java
@@ -11,18 +11,31 @@ public class Main {
 		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
 		UserDao dao = context.getBean("userDao", UserDao.class);
 		
-		User user = new User();
-		user.setId("whiteship");
-		user.setName("백기선");
-		user.setPassword("married");
+//		User user = new User();
+//		user.setId("whiteship");
+//		user.setName("백기선");
+//		user.setPassword("married");
+//		
+//		dao.add(user);
+//		
+//		System.out.println(user.getId() + " 등록 성공");
+//		
+//		User user2 = dao.get(user.getId());
+//		System.out.println(user2.getName());
+//		System.out.println(user2.getPassword());
+//		System.out.println(user2.getId() + " 조회 성공");
+//		
+		DaoFactory daoFactory = new DaoFactory();
+		UserDao dao1 = daoFactory.userDao();
+		UserDao dao2 = daoFactory.userDao();
 		
-		dao.add(user);
+		System.out.println(dao1);
+		System.out.println(dao2);
 		
-		System.out.println(user.getId() + " 등록 성공");
+		UserDao dao3 = context.getBean("userDao", UserDao.class);
+		UserDao dao4 = context.getBean("userDao", UserDao.class);
 		
-		User user2 = dao.get(user.getId());
-		System.out.println(user2.getName());
-		System.out.println(user2.getPassword());
-		System.out.println(user2.getId() + " 조회 성공");
+		System.out.println(dao3);
+		System.out.println(dao4);
 	}
 }
