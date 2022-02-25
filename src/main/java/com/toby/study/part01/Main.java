2958613b21b295d29669dfbb13f929ebb936aeb5
eodevelop <eodevelop@naver.com>
2022-02-25 14:22:39 +0900

    1.3.3 관계설정 책임의 분리 - 생성자에서 ConnectionMaker의 object를 전달해줌으로써  UserDao에서 불필요한 의존관계를 main으로 이동 가능하게된다.

--- src/main/java/com/toby/study/part01/Main.java
+++ src/main/java/com/toby/study/part01/Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new UserDao();
+		UserDao dao = new UserDao(new DConnectionMaker());
 		
 		User user = new User();
 		user.setId("whiteship");
