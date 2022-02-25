ee0e17abeb78019199d9ff9f3ba25df844417424
eodevelop <eodevelop@naver.com>
2022-02-25 14:49:18 +0900

    1.4.1 오브젝트 팩토리 - Main에 떠넘겼던 어떤 ConnectionMaker를 사용할지에대한 기능을 ObjectFactory에게 담당하게 한다. - 팩토리는 어플리케이션의 오브젝트를 구성하고 그 관계를 정의하는 책임을 맡는다.

--- src/main/java/com/toby/study/part01/Main.java
+++ src/main/java/com/toby/study/part01/Main.java
@@ -5,7 +5,7 @@ import java.sql.SQLException;
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new UserDao(new DConnectionMaker());
+		UserDao dao = new DaoFactory().userDao();
 		
 		User user = new User();
 		user.setId("whiteship");
