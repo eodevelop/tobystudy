562da3ff8c90770435a6ac2818d7aa29dc68aaff
eodevelop <eodevelop@naver.com>
2022-02-25 11:33:20 +0900

    1. 상속을 통한 확장 - 추상 클래스를 기존 DAO에 적용하여 여러가지 DB에 커넥션 될수 있도록 기능을 확장함

---
+++ src/main/java/com/toby/study/part01/DUserDao.java
@@ -0,0 +1,16 @@
+package com.toby.study.part01;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DUserDao extends UserDao{
+	
+	@Override
+	public Connection getConnection()  throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.jdbc.Driver");
+		Connection c = DriverManager.getConnection("jdbc:mysql://localhost/springbook", "spring", "book");
+		
+		return c;
+	}
+}
