25307fe226a76cddabd285380083c06637aef94d
eodevelop <eodevelop@naver.com>
2022-02-25 15:38:59 +0900

    1.5.1 오브젝트 팩토리를 이용한 스프링 IoC - SpringFramework 의존성을 추가해줌 - userDao 객체를 생성하는 메소드를 Bean으로 선언해줌으로써 userDao를 스프링 컨텍스트가 관리하게 되어 해당 객체를 가지고 올 수 있게된다.

--- src/main/java/com/toby/study/part01/Main.java
+++ src/main/java/com/toby/study/part01/Main.java
@@ -2,10 +2,14 @@ package com.toby.study.part01;
 
 import java.sql.SQLException;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
-		UserDao dao = new DaoFactory().userDao();
+		ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+		UserDao dao = context.getBean("userDao", UserDao.class);
 		
 		User user = new User();
 		user.setId("whiteship");
